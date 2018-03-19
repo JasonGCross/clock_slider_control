d6ffb96891fab2ac13bf96222a2e89181dea2bfa
Jason Cross <jasonc@slatestudio.com>
2018-03-19 12:02:13 +1100

    Initial Commit

---
+++ clock_slider_view/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clock_slider_view
+//
+//  Created by Jason Cross on 3/19/18.
+//  Copyright © 2018 Cross Swim Training, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ clock_slider_viewTests/clock_slider_viewTests.swift
@@ -0,0 +1,36 @@
+//
+//  clock_slider_viewTests.swift
+//  clock_slider_viewTests
+//
+//  Created by Jason Cross on 3/19/18.
+//  Copyright © 2018 Cross Swim Training, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import clock_slider_view
+
+class clock_slider_viewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
